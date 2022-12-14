f5dece44e8220ba5a0260647ece7bdf6992c67bd
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-14 11:50:52 +0000

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t c = 0, a = 0, r;
+size_t c = 0, a = 0;
 if (tree == NULL){
 return 0;}
 if (tree->left != NULL || tree->right != NULL){
