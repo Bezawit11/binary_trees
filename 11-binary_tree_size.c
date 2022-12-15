415912f2482e1a0d442a88d8631d4b6975dcfb82
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-15 08:46:55 +0000

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,10 +11,14 @@ size_t binary_tree_helper(const binary_tree_t *tree)
 size_t c = 0, a = 0;
 if (tree == NULL){
 return 0;}
-if (tree->left != NULL){
-c = 1 + binary_tree_helper(tree->left);}
-if (tree->right != NULL){
-a = 1 + binary_tree_helper(tree->right);}
+if (tree->left != NULL)
+{
+c = 1 + binary_tree_helper(tree->left);
+}
+if (tree->right != NULL)
+{
+a = 1 + binary_tree_helper(tree->right);
+}
 return c + a;
 }
 
@@ -26,6 +30,8 @@ return c + a;
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 size_t i = 0;
+if (tree == NULL)
+return 0;
 i = 1 + binary_tree_helper(tree);
 return i;
 }
