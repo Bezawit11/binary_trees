ecf1bd61fb9f1b48b2680734cf1efed8b8315b5f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-16 13:16:14 +0000

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,20 +7,21 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 int a;
-if (node == NULL || node->parent == NULL || node->parent->parent == NULL){
-return NULL;
-}
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return (NULL);
 a = node->parent->n;
 node = node->parent->parent;
-if (node->left != NULL && node->right != NULL){
-if (node->left->n == a){
+if (node->left != NULL && node->right != NULL)
+{
+if (node->left->n == a)
+{
 node = node->right;
-return node;
+return (node);
 }
 else
 {
 node = node->left;
-return node;
+return (node);
 }
 }
 }
