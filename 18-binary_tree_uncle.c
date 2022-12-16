7194072c76a0528d152ff93bbea4a6e7e1c45680
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-12-16 13:39:54 +0000

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
+ *binary_tree_uncle - a function that finds the uncle of a node
+ *@node: a pointer to the node to find the uncle
+ *Return: returns a pointer to the uncle node
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
@@ -24,4 +25,5 @@ node = node->left;
 return (node);
 }
 }
+return (NULL);
 }
